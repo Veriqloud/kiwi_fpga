0ca2d86634762b7439d133229c0582f5215323c0
hopdinh-95 <hoptuyen2010@gmail.com>
2025-03-24 14:48:42 +0100

    add Top wrapper

--- ABC/srcs/Bob_top_wrapper.v
+++ ABC/srcs/Bob_top_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
-//Date        : Thu Nov  7 12:11:17 2024
+//Date        : Fri Mar 21 11:19:59 2025
 //Host        : hop-MS-7D67 running 64-bit Ubuntu 22.04.5 LTS
 //Command     : generate_target Bob_top_wrapper.bd
 //Design      : Bob_top_wrapper
@@ -24,49 +24,32 @@ module Bob_top_wrapper
     c0_ddr4_dqs_t,
     c0_ddr4_odt,
     c0_ddr4_reset_n,
+    com_ext_spi_dl_miso_io,
+    com_ext_spi_dl_mosi_io,
+    com_ext_spi_dl_sck_io,
+    com_ext_spi_dl_ss_io,
+    com_ext_spi_tdc_miso_io,
+    com_ext_spi_tdc_mosi_io,
+    com_ext_spi_tdc_sck_io,
+    com_ext_spi_tdc_ss_io,
+    cr_ext_cr_ext_clk100_n,
+    cr_ext_cr_ext_clk100_p,
+    cr_ext_cr_ext_clk10_n,
+    cr_ext_cr_ext_clk10_p,
+    cr_ext_cr_fastdac_refclki_n,
+    cr_ext_cr_fastdac_refclki_p,
+    cr_ext_cr_fastdac_syncout_n,
+    cr_ext_cr_fastdac_syncout_p,
+    cr_ext_cr_fastdac_sysref_n,
+    cr_ext_cr_fastdac_sysref_p,
     ddr_sys_clk_n,
     ddr_sys_clk_p,
-    ext_clk100_n,
-    ext_clk100_p,
-    ext_clk10_n,
-    ext_clk10_p,
     ext_clk200_o,
-    ext_dac_ltc_miso,
-    ext_dac_ltc_mosi,
-    ext_dac_ltc_sck,
-    ext_dac_ltc_ss,
-    ext_fastdac_refclk_n,
-    ext_fastdac_refclk_p,
-    ext_fastdac_sync_n,
-    ext_fastdac_sync_p,
-    ext_fastdac_sysref_n,
-    ext_fastdac_sysref_p,
     ext_fastdac_txn_out,
     ext_fastdac_txp_out,
     ext_pps,
-    ext_rst_jic,
     ext_stopa_sim,
     ext_sync_ltc,
-    ext_tdc_frame_A_n,
-    ext_tdc_frame_A_p,
-    ext_tdc_frame_B_n,
-    ext_tdc_frame_B_p,
-    ext_tdc_lclki_n,
-    ext_tdc_lclki_p,
-    ext_tdc_lclko_n,
-    ext_tdc_lclko_p,
-    ext_tdc_miso,
-    ext_tdc_mosi,
-    ext_tdc_refclk_n,
-    ext_tdc_refclk_p,
-    ext_tdc_rstidxp_n,
-    ext_tdc_rstidxp_p,
-    ext_tdc_sck,
-    ext_tdc_sdi_A_n,
-    ext_tdc_sdi_A_p,
-    ext_tdc_sdi_B_n,
-    ext_tdc_sdi_B_p,
-    ext_tdc_ss,
     fifo_gc_out_rst,
     led,
     linterrupt_i,
@@ -85,9 +68,26 @@ module Bob_top_wrapper
     rd_en_fifo_gc,
     rd_gc_valid,
     read_done,
+    rst_jic,
     sys_clk_n,
     sys_clk_p,
-    sys_rst_n);
+    sys_rst_n,
+    tdc_ext_clkrst_tdc_lclki_n,
+    tdc_ext_clkrst_tdc_lclki_p,
+    tdc_ext_clkrst_tdc_refclk_n,
+    tdc_ext_clkrst_tdc_refclk_p,
+    tdc_ext_clkrst_tdc_rstidxp_n,
+    tdc_ext_clkrst_tdc_rstidxp_p,
+    tdc_ext_in_framea_n,
+    tdc_ext_in_framea_p,
+    tdc_ext_in_frameb_n,
+    tdc_ext_in_frameb_p,
+    tdc_ext_in_lclk_n,
+    tdc_ext_in_lclk_p,
+    tdc_ext_in_sdia_n,
+    tdc_ext_in_sdia_p,
+    tdc_ext_in_sdib_n,
+    tdc_ext_in_sdib_p);
   output c0_ddr4_act_n;
   output [16:0]c0_ddr4_adr;
   output [1:0]c0_ddr4_ba;
@@ -102,49 +102,32 @@ module Bob_top_wrapper
   inout [3:0]c0_ddr4_dqs_t;
   output c0_ddr4_odt;
   output c0_ddr4_reset_n;
+  inout com_ext_spi_dl_miso_io;
+  inout com_ext_spi_dl_mosi_io;
+  inout com_ext_spi_dl_sck_io;
+  inout [2:0]com_ext_spi_dl_ss_io;
+  inout com_ext_spi_tdc_miso_io;
+  inout com_ext_spi_tdc_mosi_io;
+  inout com_ext_spi_tdc_sck_io;
+  inout [1:0]com_ext_spi_tdc_ss_io;
+  input cr_ext_cr_ext_clk100_n;
+  input cr_ext_cr_ext_clk100_p;
+  input cr_ext_cr_ext_clk10_n;
+  input cr_ext_cr_ext_clk10_p;
+  input cr_ext_cr_fastdac_refclki_n;
+  input cr_ext_cr_fastdac_refclki_p;
+  input cr_ext_cr_fastdac_syncout_n;
+  input cr_ext_cr_fastdac_syncout_p;
+  input cr_ext_cr_fastdac_sysref_n;
+  input cr_ext_cr_fastdac_sysref_p;
   input ddr_sys_clk_n;
   input ddr_sys_clk_p;
-  input ext_clk100_n;
-  input ext_clk100_p;
-  input ext_clk10_n;
-  input ext_clk10_p;
   output ext_clk200_o;
-  inout ext_dac_ltc_miso;
-  inout ext_dac_ltc_mosi;
-  inout ext_dac_ltc_sck;
-  inout [2:0]ext_dac_ltc_ss;
-  input ext_fastdac_refclk_n;
-  input ext_fastdac_refclk_p;
-  input ext_fastdac_sync_n;
-  input ext_fastdac_sync_p;
-  input ext_fastdac_sysref_n;
-  input ext_fastdac_sysref_p;
   output [3:0]ext_fastdac_txn_out;
   output [3:0]ext_fastdac_txp_out;
   input ext_pps;
-  output ext_rst_jic;
   output ext_stopa_sim;
   output ext_sync_ltc;
-  input ext_tdc_frame_A_n;
-  input ext_tdc_frame_A_p;
-  input ext_tdc_frame_B_n;
-  input ext_tdc_frame_B_p;
-  output ext_tdc_lclki_n;
-  output ext_tdc_lclki_p;
-  input ext_tdc_lclko_n;
-  input ext_tdc_lclko_p;
-  inout ext_tdc_miso;
-  inout ext_tdc_mosi;
-  output ext_tdc_refclk_n;
-  output ext_tdc_refclk_p;
-  output ext_tdc_rstidxp_n;
-  output ext_tdc_rstidxp_p;
-  inout ext_tdc_sck;
-  input ext_tdc_sdi_A_n;
-  input ext_tdc_sdi_A_p;
-  input ext_tdc_sdi_B_n;
-  input ext_tdc_sdi_B_p;
-  inout [1:0]ext_tdc_ss;
   output fifo_gc_out_rst;
   output [0:0]led;
   input linterrupt_i;
@@ -163,9 +146,26 @@ module Bob_top_wrapper
   output rd_en_fifo_gc;
   output rd_gc_valid;
   output read_done;
+  output rst_jic;
   input sys_clk_n;
   input sys_clk_p;
   input sys_rst_n;
+  output tdc_ext_clkrst_tdc_lclki_n;
+  output tdc_ext_clkrst_tdc_lclki_p;
+  output tdc_ext_clkrst_tdc_refclk_n;
+  output tdc_ext_clkrst_tdc_refclk_p;
+  output tdc_ext_clkrst_tdc_rstidxp_n;
+  output tdc_ext_clkrst_tdc_rstidxp_p;
+  input tdc_ext_in_framea_n;
+  input tdc_ext_in_framea_p;
+  input tdc_ext_in_frameb_n;
+  input tdc_ext_in_frameb_p;
+  input tdc_ext_in_lclk_n;
+  input tdc_ext_in_lclk_p;
+  input tdc_ext_in_sdia_n;
+  input tdc_ext_in_sdia_p;
+  input tdc_ext_in_sdib_n;
+  input tdc_ext_in_sdib_p;
 
   wire c0_ddr4_act_n;
   wire [16:0]c0_ddr4_adr;
@@ -181,49 +181,32 @@ module Bob_top_wrapper
   wire [3:0]c0_ddr4_dqs_t;
   wire c0_ddr4_odt;
   wire c0_ddr4_reset_n;
+  wire com_ext_spi_dl_miso_io;
+  wire com_ext_spi_dl_mosi_io;
+  wire com_ext_spi_dl_sck_io;
+  wire [2:0]com_ext_spi_dl_ss_io;
+  wire com_ext_spi_tdc_miso_io;
+  wire com_ext_spi_tdc_mosi_io;
+  wire com_ext_spi_tdc_sck_io;
+  wire [1:0]com_ext_spi_tdc_ss_io;
+  wire cr_ext_cr_ext_clk100_n;
+  wire cr_ext_cr_ext_clk100_p;
+  wire cr_ext_cr_ext_clk10_n;
+  wire cr_ext_cr_ext_clk10_p;
+  wire cr_ext_cr_fastdac_refclki_n;
+  wire cr_ext_cr_fastdac_refclki_p;
+  wire cr_ext_cr_fastdac_syncout_n;
+  wire cr_ext_cr_fastdac_syncout_p;
+  wire cr_ext_cr_fastdac_sysref_n;
+  wire cr_ext_cr_fastdac_sysref_p;
   wire ddr_sys_clk_n;
   wire ddr_sys_clk_p;
-  wire ext_clk100_n;
-  wire ext_clk100_p;
-  wire ext_clk10_n;
-  wire ext_clk10_p;
   wire ext_clk200_o;
-  wire ext_dac_ltc_miso;
-  wire ext_dac_ltc_mosi;
-  wire ext_dac_ltc_sck;
-  wire [2:0]ext_dac_ltc_ss;
-  wire ext_fastdac_refclk_n;
-  wire ext_fastdac_refclk_p;
-  wire ext_fastdac_sync_n;
-  wire ext_fastdac_sync_p;
-  wire ext_fastdac_sysref_n;
-  wire ext_fastdac_sysref_p;
   wire [3:0]ext_fastdac_txn_out;
   wire [3:0]ext_fastdac_txp_out;
   wire ext_pps;
-  wire ext_rst_jic;
   wire ext_stopa_sim;
   wire ext_sync_ltc;
-  wire ext_tdc_frame_A_n;
-  wire ext_tdc_frame_A_p;
-  wire ext_tdc_frame_B_n;
-  wire ext_tdc_frame_B_p;
-  wire ext_tdc_lclki_n;
-  wire ext_tdc_lclki_p;
-  wire ext_tdc_lclko_n;
-  wire ext_tdc_lclko_p;
-  wire ext_tdc_miso;
-  wire ext_tdc_mosi;
-  wire ext_tdc_refclk_n;
-  wire ext_tdc_refclk_p;
-  wire ext_tdc_rstidxp_n;
-  wire ext_tdc_rstidxp_p;
-  wire ext_tdc_sck;
-  wire ext_tdc_sdi_A_n;
-  wire ext_tdc_sdi_A_p;
-  wire ext_tdc_sdi_B_n;
-  wire ext_tdc_sdi_B_p;
-  wire [1:0]ext_tdc_ss;
   wire fifo_gc_out_rst;
   wire [0:0]led;
   wire linterrupt_i;
@@ -242,9 +225,26 @@ module Bob_top_wrapper
   wire rd_en_fifo_gc;
   wire rd_gc_valid;
   wire read_done;
+  wire rst_jic;
   wire sys_clk_n;
   wire sys_clk_p;
   wire sys_rst_n;
+  wire tdc_ext_clkrst_tdc_lclki_n;
+  wire tdc_ext_clkrst_tdc_lclki_p;
+  wire tdc_ext_clkrst_tdc_refclk_n;
+  wire tdc_ext_clkrst_tdc_refclk_p;
+  wire tdc_ext_clkrst_tdc_rstidxp_n;
+  wire tdc_ext_clkrst_tdc_rstidxp_p;
+  wire tdc_ext_in_framea_n;
+  wire tdc_ext_in_framea_p;
+  wire tdc_ext_in_frameb_n;
+  wire tdc_ext_in_frameb_p;
+  wire tdc_ext_in_lclk_n;
+  wire tdc_ext_in_lclk_p;
+  wire tdc_ext_in_sdia_n;
+  wire tdc_ext_in_sdia_p;
+  wire tdc_ext_in_sdib_n;
+  wire tdc_ext_in_sdib_p;
 
   Bob_top Bob_top_i
        (.c0_ddr4_act_n(c0_ddr4_act_n),
@@ -261,49 +261,32 @@ module Bob_top_wrapper
         .c0_ddr4_dqs_t(c0_ddr4_dqs_t),
         .c0_ddr4_odt(c0_ddr4_odt),
         .c0_ddr4_reset_n(c0_ddr4_reset_n),
+        .com_ext_spi_dl_miso_io(com_ext_spi_dl_miso_io),
+        .com_ext_spi_dl_mosi_io(com_ext_spi_dl_mosi_io),
+        .com_ext_spi_dl_sck_io(com_ext_spi_dl_sck_io),
+        .com_ext_spi_dl_ss_io(com_ext_spi_dl_ss_io),
+        .com_ext_spi_tdc_miso_io(com_ext_spi_tdc_miso_io),
+        .com_ext_spi_tdc_mosi_io(com_ext_spi_tdc_mosi_io),
+        .com_ext_spi_tdc_sck_io(com_ext_spi_tdc_sck_io),
+        .com_ext_spi_tdc_ss_io(com_ext_spi_tdc_ss_io),
+        .cr_ext_cr_ext_clk100_n(cr_ext_cr_ext_clk100_n),
+        .cr_ext_cr_ext_clk100_p(cr_ext_cr_ext_clk100_p),
+        .cr_ext_cr_ext_clk10_n(cr_ext_cr_ext_clk10_n),
+        .cr_ext_cr_ext_clk10_p(cr_ext_cr_ext_clk10_p),
+        .cr_ext_cr_fastdac_refclki_n(cr_ext_cr_fastdac_refclki_n),
+        .cr_ext_cr_fastdac_refclki_p(cr_ext_cr_fastdac_refclki_p),
+        .cr_ext_cr_fastdac_syncout_n(cr_ext_cr_fastdac_syncout_n),
+        .cr_ext_cr_fastdac_syncout_p(cr_ext_cr_fastdac_syncout_p),
+        .cr_ext_cr_fastdac_sysref_n(cr_ext_cr_fastdac_sysref_n),
+        .cr_ext_cr_fastdac_sysref_p(cr_ext_cr_fastdac_sysref_p),
         .ddr_sys_clk_n(ddr_sys_clk_n),
         .ddr_sys_clk_p(ddr_sys_clk_p),
-        .ext_clk100_n(ext_clk100_n),
-        .ext_clk100_p(ext_clk100_p),
-        .ext_clk10_n(ext_clk10_n),
-        .ext_clk10_p(ext_clk10_p),
         .ext_clk200_o(ext_clk200_o),
-        .ext_dac_ltc_miso(ext_dac_ltc_miso),
-        .ext_dac_ltc_mosi(ext_dac_ltc_mosi),
-        .ext_dac_ltc_sck(ext_dac_ltc_sck),
-        .ext_dac_ltc_ss(ext_dac_ltc_ss),
-        .ext_fastdac_refclk_n(ext_fastdac_refclk_n),
-        .ext_fastdac_refclk_p(ext_fastdac_refclk_p),
-        .ext_fastdac_sync_n(ext_fastdac_sync_n),
-        .ext_fastdac_sync_p(ext_fastdac_sync_p),
-        .ext_fastdac_sysref_n(ext_fastdac_sysref_n),
-        .ext_fastdac_sysref_p(ext_fastdac_sysref_p),
         .ext_fastdac_txn_out(ext_fastdac_txn_out),
         .ext_fastdac_txp_out(ext_fastdac_txp_out),
         .ext_pps(ext_pps),
-        .ext_rst_jic(ext_rst_jic),
         .ext_stopa_sim(ext_stopa_sim),
         .ext_sync_ltc(ext_sync_ltc),
-        .ext_tdc_frame_A_n(ext_tdc_frame_A_n),
-        .ext_tdc_frame_A_p(ext_tdc_frame_A_p),
-        .ext_tdc_frame_B_n(ext_tdc_frame_B_n),
-        .ext_tdc_frame_B_p(ext_tdc_frame_B_p),
-        .ext_tdc_lclki_n(ext_tdc_lclki_n),
-        .ext_tdc_lclki_p(ext_tdc_lclki_p),
-        .ext_tdc_lclko_n(ext_tdc_lclko_n),
-        .ext_tdc_lclko_p(ext_tdc_lclko_p),
-        .ext_tdc_miso(ext_tdc_miso),
-        .ext_tdc_mosi(ext_tdc_mosi),
-        .ext_tdc_refclk_n(ext_tdc_refclk_n),
-        .ext_tdc_refclk_p(ext_tdc_refclk_p),
-        .ext_tdc_rstidxp_n(ext_tdc_rstidxp_n),
-        .ext_tdc_rstidxp_p(ext_tdc_rstidxp_p),
-        .ext_tdc_sck(ext_tdc_sck),
-        .ext_tdc_sdi_A_n(ext_tdc_sdi_A_n),
-        .ext_tdc_sdi_A_p(ext_tdc_sdi_A_p),
-        .ext_tdc_sdi_B_n(ext_tdc_sdi_B_n),
-        .ext_tdc_sdi_B_p(ext_tdc_sdi_B_p),
-        .ext_tdc_ss(ext_tdc_ss),
         .fifo_gc_out_rst(fifo_gc_out_rst),
         .led(led),
         .linterrupt_i(linterrupt_i),
@@ -322,7 +305,24 @@ module Bob_top_wrapper
         .rd_en_fifo_gc(rd_en_fifo_gc),
         .rd_gc_valid(rd_gc_valid),
         .read_done(read_done),
+        .rst_jic(rst_jic),
         .sys_clk_n(sys_clk_n),
         .sys_clk_p(sys_clk_p),
-        .sys_rst_n(sys_rst_n));
+        .sys_rst_n(sys_rst_n),
+        .tdc_ext_clkrst_tdc_lclki_n(tdc_ext_clkrst_tdc_lclki_n),
+        .tdc_ext_clkrst_tdc_lclki_p(tdc_ext_clkrst_tdc_lclki_p),
+        .tdc_ext_clkrst_tdc_refclk_n(tdc_ext_clkrst_tdc_refclk_n),
+        .tdc_ext_clkrst_tdc_refclk_p(tdc_ext_clkrst_tdc_refclk_p),
+        .tdc_ext_clkrst_tdc_rstidxp_n(tdc_ext_clkrst_tdc_rstidxp_n),
+        .tdc_ext_clkrst_tdc_rstidxp_p(tdc_ext_clkrst_tdc_rstidxp_p),
+        .tdc_ext_in_framea_n(tdc_ext_in_framea_n),
+        .tdc_ext_in_framea_p(tdc_ext_in_framea_p),
+        .tdc_ext_in_frameb_n(tdc_ext_in_frameb_n),
+        .tdc_ext_in_frameb_p(tdc_ext_in_frameb_p),
+        .tdc_ext_in_lclk_n(tdc_ext_in_lclk_n),
+        .tdc_ext_in_lclk_p(tdc_ext_in_lclk_p),
+        .tdc_ext_in_sdia_n(tdc_ext_in_sdia_n),
+        .tdc_ext_in_sdia_p(tdc_ext_in_sdia_p),
+        .tdc_ext_in_sdib_n(tdc_ext_in_sdib_n),
+        .tdc_ext_in_sdib_p(tdc_ext_in_sdib_p));
 endmodule
